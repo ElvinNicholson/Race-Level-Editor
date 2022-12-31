a4295f2dd768d238f2c9cbaac5a8a3a4703820e5
Elvin Nicholosn <Elvin2.Nicholson@live.uwe.ac.uk>
2022-12-31 15:12:52 +0000

    Added WPF Project

---
+++ JSON Level Editor/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace JSON_Level_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Triggers when File > Open is clicked
+        /// </summary>
+        private void fileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            if (openFileDialog.ShowDialog() == true) 
+            {
+                string filename = openFileDialog.FileName;
+                MessageBox.Show(filename);
+            }
+        }
+
+    }
+}
